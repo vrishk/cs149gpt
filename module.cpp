4a132bf145ce18ee955c688ce44cc978a5b027dd
Cary Xiao <caryxiao@stanford.edu>
2023-12-02 16:00:02 -0800

    wip: part3 ispc now works

--- module.cpp
+++ module.cpp
@@ -185,28 +185,55 @@ torch::Tensor myFusedAttention(torch::Tensor QTensor, torch::Tensor KTensor, tor
       //loop over heads
       for (int h = 0; h < H; h++){
         for (int i = 0; i < N ; i++){
+		      // // YRow is moved inside so each OpenMP thread gets a local copy.
+          // at::Tensor ORowTensor = temp.index({torch::indexing::Slice(omp_get_thread_num(), torch::indexing::None)});      
+          // std::vector<float> QK_t= formatTensor(ORowTensor);
+          // std::fill(QK_t.begin(), QK_t.end(), 0);
+          // float val;
+          // fusedMatrixMult(Q.data(), K.data(), QK_t.data(), B, H, N, d, b, h, BLOCK, i);
+          // fusedSoftmaxNorm(QK_t.data(), N, i);
+          // // fusedPvCalc(QK_t.data(), V.data(), O.data(), B, H, N, d, b, h, BLOCK, i);
+
 		      // YRow is moved inside so each OpenMP thread gets a local copy.
           at::Tensor ORowTensor = temp.index({torch::indexing::Slice(omp_get_thread_num(), torch::indexing::None)});      
           std::vector<float> QK_t= formatTensor(ORowTensor);
           std::fill(QK_t.begin(), QK_t.end(), 0);
+
           float val;
           fusedMatrixMult(Q.data(), K.data(), QK_t.data(), B, H, N, d, b, h, BLOCK, i);
           fusedSoftmaxNorm(QK_t.data(), N, i);
-          // fusedPvCalc(QK_t.data(), V.data(), O.data(), B, H, N, d, b, h, BLOCK, i);
+          fusedPvCalc(QK_t.data(), V.data(), O.data(), B, H, N, d, b, h, BLOCK, i);
+          // val = 0.f;
+          // float sample, normed;
+          // float min_val = 0.f;
+          // if (USE_MIN_NUMERIC) {
+          //   min_val = std::numeric_limits<float>::max();;
+          //   for (int j = 0; j < N; j++) {
+          //     sample = QK_t[j];
+          //     if (sample < min_val) {min_val = sample;}
+          //   }
+          // }
+          // for (int j = 0; j < N; j++) {
+          //   val += exp(QK_t[j] - min_val);
+          // }
+          // for (int j = 0; j < N; j++) {
+          //   QK_t[j] = exp(QK_t[j] - min_val) / val;
+          // }
 
-          for (int kk = 0; kk < d; kk += BLOCK) {
-            uint BLOCK_K = min(kk + BLOCK, d);
-            for (int jj = 0; jj < N; jj += BLOCK) {
-              uint BLOCK_J = min(jj + BLOCK, N);
-              for(int k = kk; k < BLOCK_K; k++){
-                val = fourDimRead(O, b, h, i, k, H, N, d);
-                for(int j = jj; j < BLOCK_J; j++){
-                  val += QK_t[j] * fourDimRead(V, b, h, j, k, H, N, d);
-                }
-                fourDimWrite(O, b, h, i, k, H, N, d, val);
-              }
-            }
-          }
+          // for (int kk = 0; kk < d; kk += BLOCK) {
+          //   uint BLOCK_K = min(kk + BLOCK, d);
+          //   for (int jj = 0; jj < N; jj += BLOCK) {
+          //     uint BLOCK_J = min(jj + BLOCK, N);
+          //     for(int k = kk; k < BLOCK_K; k++){
+          //       val = fourDimRead(O, b, h, i, k, H, N, d);
+          //       for(int j = jj; j < BLOCK_J; j++){
+          //         val += QK_t[j] * fourDimRead(V, b, h, j, k, H, N, d);
+          //       }
+          //       fourDimWrite(O, b, h, i, k, H, N, d, val);
+          //     }
+          //   }
+          // }
+
         }
 	    }
     }
